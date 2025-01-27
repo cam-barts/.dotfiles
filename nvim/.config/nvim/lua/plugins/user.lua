63f3a38b1f31b0d5c3b5710ff53e020d07faacf4
Cam Barts <camerond.barts@gmail.com>
2025-01-27 14:48:30 -0500

    Updated astronvim

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -1,3 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+---@type LazySpec
 return {
   { "wakatime/vim-wakatime", lazy = false },
   {
--- nvim/.config/nvim/lua/user/options.lua
+++
@@ -1,5 +0,0 @@
-return {
-  opt = {
-    colorcolumn = "88",
-  },
-}
